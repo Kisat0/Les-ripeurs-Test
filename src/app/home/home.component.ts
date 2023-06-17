ba63cc7dc1ca94345f31178b2c3a10d3cc921aa1
Kisat0 <ghanembenyoucef@gmail.com>
2023-06-17 19:15:05 +0200

    🎊 - Finish icon custom exercise

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'icon-test',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/Les_Ripeurs_LogoTest.svg')
+    );
+  }
 
   ngOnInit(): void {
   }
